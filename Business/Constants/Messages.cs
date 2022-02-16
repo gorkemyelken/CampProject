d7485d1d8903f452c8efbafbf0ea4e4e81b827ec
Görkem <gorkem_yelken@hotmail.com>
2022-02-16 17:01:10 +0300

    Day 13

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -9,8 +9,12 @@ namespace Business.Constants
     public static class Messages
     {
         public static string ProductAdded = "Product added.";
+        public static string ProductUpdated = "Product updated.";
         public static string ProductNameInvalid = "Product name is invalid.";
         public static string MaintenanceTime = "Products are not listed. System in maintenance.";
         public static string ProductsListed = "Products are listed.";
+        public static string ProductCountOfCategoryError = "Error";
+        public static string ProductNameAlreadyExists = "Product name already exists.";
+        public static string CategoryLimitExceed = "Category limit is exceed.";
     }
 }
